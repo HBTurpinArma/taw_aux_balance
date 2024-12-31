a28899835a181c194b02cc719102132e8a9d2034
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2024-12-31 19:54:25 +0000

    Initial commit

---
+++ CfgPatches.hpp
@@ -0,0 +1,14 @@
+class cfgPatches
+{
+	class TAW_AUX_BALANCE
+	{
+		name="TAW Aux Balance";
+		author="TAW";
+		requiredAddons[]={};
+		requiredVersion=1.0;
+		weapons[]={};
+		units[]={};
+		magazines[]={};
+		ammo[]={};
+	};
+};
