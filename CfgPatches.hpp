6e710e5225350a046f25081c3adef5e340817526
Harrison Turpin <harrison.bradley.turpin@gmail.com>
2025-01-18 22:00:50 +0000

    Adjust Addon Dependencies

--- CfgPatches.hpp
+++ CfgPatches.hpp
@@ -4,7 +4,17 @@ class cfgPatches
 	{
 		name="TAW Aux Balance";
 		author="TAW";
-		requiredAddons[]={};
+		requiredAddons[]={
+			"A3_Data_F",
+			"A3_Functions_F",
+			"A3_Air_F",
+			"A3_Weapons_F",
+			"A3_Weapons_F_Tank",
+			"A3_Weapons_F_Sams",
+			"A3_Weapons_F_Enoch",
+			"rhsusf_main",
+			"rhsusf_melb",
+		};
 		requiredVersion=1.0;
 		weapons[]={};
 		units[]={};
